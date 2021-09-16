d9bcf04eb0936fe281408afa691099bda81e8cb1
Mark Cirineo <88806942+MarkCirineo@users.noreply.github.com>
2021-09-16 18:16:46 -0400

    added function to save items to local storage on click of respect button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 currentDay = $("#currentDay");
 currentDay.text(moment().format("dddd, MMMM Do, YYYY, HH:mm:ss"));
 
+//initial function that runs on page load
 function init() {
     setInterval(() => {
         currentDay.text(moment().format("dddd, MMMM Do, YYYY, HH:mm:ss"));
@@ -10,3 +11,58 @@ function init() {
 
 init();
 
+//textarea values for object
+nineItem = $("#nine");
+tenItem = $("#ten");
+elevenItem = $("#eleven");
+twelveItem = $("#twelve");
+oneItem = $("#one");
+twoItem = $("#two");
+threeItem = $("#three");
+fourItem = $("#four");
+fiveItem = $("#five");
+
+//object for local storage
+var scheduleItems = {
+    nine: nineItem.val(),
+    ten: tenItem.val(),
+    eleven: elevenItem.val(),
+    twelve: twelveItem.val(),
+    one: oneItem.val(),
+    two: twoItem.val(),
+    three: threeItem.val(),
+    four: fourItem.val(),
+    five: fiveItem.val()
+};
+
+// saving items to local storage on button click
+function saveScheduleItems(event) {
+    var saveButton = event.target;
+    if (saveButton.matches(".save-nine")) {
+        scheduleItems.nine = nineItem.val()
+    } else if (saveButton.matches(".save-ten")) {
+        scheduleItems.ten = tenItem.val()
+    } else if (saveButton.matches(".save-eleven")) {
+        scheduleItems.eleven = elevenItem.val()
+    } else if (saveButton.matches(".save-twelve")) {
+        scheduleItems.twelve = twelveItem.val()
+    } else if (saveButton.matches(".save-one")) {
+        scheduleItems.one = oneItem.val()
+    } else if (saveButton.matches(".save-two")) {
+        scheduleItems.two = twoItem.val()
+    } else if (saveButton.matches(".save-three")) {
+        scheduleItems.three = threeItem.val()
+    } else if (saveButton.matches(".save-four")) {
+        scheduleItems.four = fourItem.val()
+    } else if (saveButton.matches(".save-five")) {
+        scheduleItems.five = fiveItem.val()
+    } 
+    localStorage.setItem("schedule items", JSON.stringify(scheduleItems));
+}
+//button click for above
+container = $(".container")
+container.on("click", saveScheduleItems)
+
+function renderScheduleItems() {
+
+}
