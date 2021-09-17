1e8cc04b69e7a92cdf57d84f716c304a176416b7
Mark Cirineo <88806942+MarkCirineo@users.noreply.github.com>
2021-09-16 21:36:11 -0400

    fixed function to display items when page loads

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,7 @@ console.log(currentHour);
 
 
 
-//textarea values for object
+// textarea values
 nineItem = $("#nine");
 tenItem = $("#ten");
 elevenItem = $("#eleven");
@@ -19,15 +19,15 @@ twoItem = $("#two");
 threeItem = $("#three");
 fourItem = $("#four");
 fiveItem = $("#five");
-// saving items to local storage on button click
 
+// selects all elements with class .saveBtn
 saveBtn = document.querySelectorAll(".saveBtn");
-console.log(saveBtn);
-
+// loops through all elements and adds an event listener to run the function
 saveBtn.forEach(function(save) {
     save.addEventListener("click", saveScheduleItems)
 })
 
+//saves items to local storage when save button is clicked
 function saveScheduleItems(event) {
     var element = event.target;
     
@@ -50,102 +50,24 @@ function saveScheduleItems(event) {
     } else if (element.matches(".save-five")) {
         localStorage.setItem(05, fiveItem.val())
     }
-
-    // console.log(saveButton)
-    // if (saveButton.matches(".save-nine") || nineItem.val() !== "") {
-    //     scheduleItems.nine = nineItem.val()
-    //     console.log(saveButton)
-    // } else if (saveButton.matches(".save-ten") || tenItem.val() !== "") {
-    //     console.log(saveButton)
-    //     scheduleItems.ten = tenItem.val()
-    // } else if (saveButton.matches(".save-eleven") || elevenItem.val() !== "") {
-    //     scheduleItems.eleven = elevenItem.val()
-    // } else if (saveButton.matches(".save-twelve") || twelveItem.val() !== "") {
-    //     scheduleItems.twelve = twelveItem.val()
-    // } else if (saveButton.matches(".save-one") || oneItem.val() !== "") {
-    //     scheduleItems.one = oneItem.val()
-    // } else if (saveButton.matches(".save-two") || twoItem.val() !== "") {
-    //     scheduleItems.two = twoItem.val()
-    // } else if (saveButton.matches(".save-three") || threeItem.val() !== "") {
-    //     scheduleItems.three = threeItem.val()
-    // } else if (saveButton.matches(".save-four") || fourItem.val() !== "") {
-    //     scheduleItems.four = fourItem.val()
-    // } else if (saveButton.matches(".save-five") || fiveItem.val() !== "") {
-    //     scheduleItems.five = fiveItem.val()
-    // } 
-    // if (saveButton.matches(".save-nine")) {
-    //     scheduleItems.nine = nineItem.val()
-    //     console.log(saveButton)
-    // } else if (saveButton.matches(".save-ten")) {
-    //     console.log(saveButton)
-    //     scheduleItems.ten = tenItem.val()
-    // } else if (saveButton.matches(".save-eleven")) {
-    //     scheduleItems.eleven = elevenItem.val()
-    // } else if (saveButton.matches(".save-twelve")) {
-    //     scheduleItems.twelve = twelveItem.val()
-    // } else if (saveButton.matches(".save-one")) {
-    //     scheduleItems.one = oneItem.val()
-    // } else if (saveButton.matches(".save-two")) {
-    //     scheduleItems.two = twoItem.val()
-    // } else if (saveButton.matches(".save-three")) {
-    //     scheduleItems.three = threeItem.val()
-    // } else if (saveButton.matches(".save-four")) {
-    //     scheduleItems.four = fourItem.val()
-    // } else if (saveButton.matches(".save-five")) {
-    //     scheduleItems.five = fiveItem.val()
-    // } 
-
-    // switch (true) {
-    //     case (saveButton.matches(".save-nine") || nineItem.val() !== ""):
-    //         scheduleItems.nine = nineItem.val();
-    //     case (saveButton.matches(".save-ten") || tenItem.val() !== ""):
-    //         scheduleItems.ten = tenItem.val();
-    //     case (saveButton.matches(".save-eleven") || elevenItem.val() !== ""):
-    //         scheduleItems.eleven = elevenItem.val();
-    //     case (saveButton.matches(".save-twelve") || twelveItem.val() !== ""):
-    //         scheduleItems.twelve = twelveItem.val();
-    //     case (saveButton.matches(".save-one") || oneItem.val() !== ""):
-    //         scheduleItems.one = oneItem.val();
-    //     case (saveButton.matches(".save-two") || twoItem.val() !== ""):
-    //         scheduleItems.two = twoItem.val();
-    //     case (saveButton.matches(".save-three") || threeItem.val() !== ""):
-    //         scheduleItems.three = threeItem.val();
-    //     case (saveButton.matches(".save-four") || fourItem.val() !== ""):
-    //         scheduleItems.four = fourItem.val();
-    //     case (saveButton.matches(".save-five") || fiveItem.val() !== ""):
-    //         scheduleItems.five = fiveItem.val();           
-    //     default:
-    //         break;
-    // }
-
-    // localStorage.setItem("schedule items", JSON.stringify(scheduleItems));
 }
-//button click for above
-// container = $(".container")
 
-// TODO: fix items not displaying when refreshing
+// shows items on screen when function is called
 function renderScheduleItems() {
-    var savedSheduleItems = JSON.parse(localStorage.getItem("schedule items"))
-    // console.log(savedSheduleItems)
-    // console.log(savedSheduleItems.nine)
-    if (savedSheduleItems !== null) {
-        nineItem.val(savedSheduleItems.nine)
-        tenItem.val(savedSheduleItems.ten)
-        elevenItem.val(savedSheduleItems.eleven)
-        twelveItem.val(savedSheduleItems.twelve)
-        oneItem.val(savedSheduleItems.one)
-        twoItem.val(savedSheduleItems.two)
-        threeItem.val(savedSheduleItems.three)
-        fourItem.val(savedSheduleItems.four)
-        fiveItem.val(savedSheduleItems.five)
-    } else {
-        return;
-    }
-   
-    // console.log(savedSheduleItems.nine)
+    nineItem.val(localStorage.getItem(09));
+    tenItem.val(localStorage.getItem(10));
+    elevenItem.val(localStorage.getItem(11));
+    twelveItem.val(localStorage.getItem(12));
+    oneItem.val(localStorage.getItem(01));
+    twoItem.val(localStorage.getItem(02));
+    threeItem.val(localStorage.getItem(03));
+    fourItem.val(localStorage.getItem(04));
+    fiveItem.val(localStorage.getItem(05));
 }
 
 //initial function that runs on page load
+// displays time at the top
+// and calls function to display items on screen
 function init() {
     setInterval(() => {
         currentDay.text(moment().format("dddd, MMMM Do, YYYY, HH:mm:ss"));
